59895451e1e2f50221e06deb6006f19bdd21c421
sumitkakshapati <me.summet@gmail.com>
2022-11-08 07:12:13 +0545

    add feature added

--- main.dart
+++ main.dart
@@ -1,3 +1,13 @@
 void main() {
-  print("Hello World");
+  int _number = 10;
+  int _number2 = 20;
+
+  final _result = Calculator.add(_number, _number2);
+  print(_result);
+}
+
+class Calculator {
+  static int add(int a, int b) {
+    return a + b;
+  }
 }
